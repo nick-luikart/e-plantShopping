77d5aab40e1d92dde6883d15aa8c0b6d61a8b852
nick-luikart <nluikart2@gmail.com>
2025-09-25 19:07:47 -0400

    fixed functionality for removing items from cart and added quantity to cart icon.

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -18,7 +18,6 @@ export const CartSlice = createSlice({
     },
     removeItem: (state, action) => {
         state.items = state.items.filter(item => item.name !== action.payload);
-        
     },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload;
